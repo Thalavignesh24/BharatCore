4065d669f7843a291f9f3d313b71e2a486056366
XY088 <vigneshk@XYCHELAP054.local>
2025-10-26 00:20:24 +0530

    Password Generator

--- frontend/user-interface/src/app/productivity-tools/passwordGenerator/page.tsx
+++ frontend/user-interface/src/app/productivity-tools/passwordGenerator/page.tsx
@@ -54,35 +54,42 @@ export default function SignupPage () {
       }
     )
     const result = await await apiCall.json()
-    setUserData(result?.['data']?.['generatedPassword'])
+    if (result?.['success'] != true || result?.['statusCode'] != 200) {
+      alert('At least one rule must be selected.✅')
+      return
+    } else {
+      setUserData(result?.['data']?.['generatedPassword'])
+    }
   }
 
   return (
-    <div className='container'>
+    <div className='main'>
       <h1 className='title'> Random Password Generator </h1>
-      <input
-        type='text'
-        className='passwordLength'
-        placeholder='Enter the Password Length'
-      />
-      <br></br> <br></br>
-      <input type='checkbox' className='lowerCase' />{' '}
-      <span>Include LoweCase (a-z)</span>
-      <br></br> <br></br>
-      <input type='checkbox' className='upperCase' value={'off'} />{' '}
-      <span>Include upperCase (A-Z)</span>
-      <br></br> <br></br>
-      <input type='checkbox' className='numberCase' />{' '}
-      <span>Include Numbers (0-9)</span>
-      <br></br> <br></br>
-      <input type='checkbox' className='specialCase' />{' '}
-      <span>Include Special Characters ('!"#$%&'()*+,-./:;?@[\]^_`~)</span>
-      <br></br> <br></br>
-      <button onClick={onSubmit}>Generate Password</button>
-      <br></br> <br></br>
-      <label>Generated Password:</label>
-      <br></br>
-      <p id='passwordResult'>{userData}</p>
+      <div className='container'>
+        <input
+          type='text'
+          className='passwordLength'
+          placeholder='Enter the Password Length'
+        />
+        <br></br> <br></br>
+        <input type='checkbox' className='lowerCase' />{' '}
+        <span>Include LoweCase (a-z)</span>
+        <br></br> <br></br>
+        <input type='checkbox' className='upperCase' value={'off'} />{' '}
+        <span>Include upperCase (A-Z)</span>
+        <br></br> <br></br>
+        <input type='checkbox' className='numberCase' />{' '}
+        <span>Include Numbers (0-9)</span>
+        <br></br> <br></br>
+        <input type='checkbox' className='specialCase' />{' '}
+        <span>Include Special Characters ('!"#$%&'()*+,-./:;?@[\]^_`~)</span>
+        <br></br> <br></br>
+        <button onClick={onSubmit}>Generate Password</button>
+        <br></br> <br></br>
+        <label>Generated Password:</label>
+        <br></br>
+        <p id='passwordResult'>{userData}</p>
+      </div>
     </div>
   )
 }
